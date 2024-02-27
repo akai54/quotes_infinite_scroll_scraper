8dca1b9322e6fda3e8286a0b5d335e21740f14d7
besher AL ADDAM <beshoux@outlook.fr>
2024-02-27 01:59:07 +0100

    Add field items

--- quotes/items.py
+++ quotes/items.py
@@ -8,5 +8,6 @@ import scrapy
 
 class QuotesItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    author = scrapy.Field()
+    quote = scrapy.Field()
+    tags = scrapy.Field()
